cdfd40431f9557775556b7f4dee9aa3d5b1fa0e8
zunda <zundan@gmail.com>
2017-05-07 19:02:52 -1000

    Make sure fakeredis is svailable

--- test/factorize_test.rb
+++ test/factorize_test.rb
@@ -1,11 +1,12 @@
 require 'test_helper'
+require 'fakeredis'
 
 class FactorizeTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Factorize::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_that_fakeredis_is_available
+    assert Redis.new
   end
 end
